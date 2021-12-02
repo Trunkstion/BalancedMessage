074a723cd983bd55bd0b2aae3581b06532f12360
BastianLillo <bastian.lillo.7311@gmail.com>
2021-12-02 16:15:30 -0300

    2.0

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -14,22 +14,18 @@ class MessageController < ApplicationController
         sad = 0
         happy = 0
         
-        
+        puts str
         if str == nil || str.length == 0
             @result = true
         else
             str.each_char do |c|
                 smile = smile + 1
-               if c == ')' && i == 0
-                    @result = false
-                    break
-               else
                     case c
                         when '('
-                            i += 1
+                            i = i + 1
                             
                         when ')'
-                            i -= 1
+                            i = i - 1
                         when ':'
                             if str.at(smile +1) == '('
                                 str.each_char do |par|
@@ -39,12 +35,6 @@ class MessageController < ApplicationController
                                         cl = cl + 1
                                     end
                                 end
-                                if op > cl
-                                    sad = sad + 1
-                                elsif op < cl
-                                    happy = happy + 1
-                                end
-
                             elsif str.at(smile +1) == ')'
                                 str.each_char do |par|
                                     if par == '('
@@ -53,26 +43,25 @@ class MessageController < ApplicationController
                                         cl = cl + 1
                                     end
                                 end
-                                if op > cl
-                                    sad = sad + 1
-                                elsif op < cl
-                                    happy = happy + 1
-                                end
+                            end
+                            
+                            if op > cl
+                                sad = sad + 1
+                            elsif op < cl
+                                happy = happy + 1
                             end
                     end
                 end
-            end
         end
-
 
        if i == 0
             @result = true
             render 'index'
-       elsif (i == sad) || (i == happy)
+       elsif (i == sad) || (i.abs == happy)
             @result = true
             render 'index'
        else
-            if i > 0
+            if i != 0
                 @result = false
                 render 'index'  
             end      
